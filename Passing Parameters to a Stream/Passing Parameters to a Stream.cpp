0cc2b726f319cd625ed2109d4579ef9a9d9b3c94
WaiperOK <ruslan.topol2@gmail.com>
2022-07-02 19:07:58 +0300

    Добавьте файлы проекта.

---
+++ Passing Parameters to a Stream/Passing Parameters to a Stream.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+#include <thread>
+
+#include <chrono>
+
+using namespace std;
+
+void DoWork(int a, int b, string msg)
+{
+	cout << msg << endl;
+
+	this_thread::sleep_for(chrono::milliseconds(3000));
+
+	cout << "==========\t" << "DoWork STARTED\t==========" << endl;
+
+	this_thread::sleep_for(chrono::milliseconds(5000));
+
+	cout << " A + B = " << a + b << endl;
+
+	this_thread::sleep_for(chrono::milliseconds(1000));
+
+	cout << "==========\t" << "DoWork STOPPED\t==========" << endl;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	thread th(DoWork, 2, 3, "our message");
+
+
+	for (size_t i = 0; i < 10; i++)
+	{
+		cout << "ID потока =" << this_thread::get_id() << "\tmain" << endl;
+		this_thread::sleep_for(chrono::milliseconds(500));
+	}
+
+	th.join(); // дожидаемся метода 
+
+	return 0;
+}
+
